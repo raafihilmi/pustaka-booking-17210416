73aa55b2c631c771a09d5ce9a106573f3cd981cc
Raafi Hilmi <raafihilmi90@gmail.com>
2024-04-23 10:31:18 +0700

    Pertemuan 4

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Raafi</li>
+ <li>Nama Belakang : Hilmi</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Jl. Cikareo Roda Hias, Tangerang Selatan</li>
+ </ul>
+
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Tangerang</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>Bulutangkis</li>
+ <li>Lari</li>
+ </ul>
+</section>
